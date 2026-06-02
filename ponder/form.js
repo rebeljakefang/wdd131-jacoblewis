e3fe0a76385ac11bfa78101e70bdb671f1ec0fe9
rebeljakefang <rebeljake16.jl@gmail.com>
2026-06-02 09:52:05 -0600

    Update form.js

--- ponder/form.js
+++ ponder/form.js
@@ -1,3 +1,10 @@
 // retriveve the form from the dom print the result 
-const
-let 
+const form = document.querySelector("#fsyform");
+
+// add an event listener to the form to detect submit
+form.addEventListener("submit", Event => {
+    Event.preventDefault();
+    console.log(Event);
+    console.log(form.firstname.value);
+//print last name and email
+}
